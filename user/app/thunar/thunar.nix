6d0b72a5225082cb5ae3769f140f4a31760fae53
jp <jpenno123@hotmail.com>
2024-04-07 16:19:15 +1000

    theme thunar and change font and font size

--- user/app/thunar/thunar.nix
+++ user/app/thunar/thunar.nix
@@ -1,5 +1,30 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, theme, ... }:
 
 {
   home.packages = with pkgs; [ xfce.thunar xfce.xfconf ];
+
+  #qtk
+  qt.enable = true;
+
+  qt.platformTheme = "gtk";
+
+  qt.style.name = "adwaita-dark";
+
+  qt.style.package = pkgs.adwaita-qt;
+
+  # gtk
+  gtk.enable = true;
+
+  gtk.theme.package = pkgs.adw-gtk3;
+  gtk.theme.name = "adw-gtk3-dark";
+
+  gtk.font.size = 14;
+  gtk.font.name = theme.font;
+
+  # gtk.cusorTheme.package = pkgs.bibata-cursors;
+  # gtk.cusorTheme.name = "Bibata-Modern-Ice";
+
+  # gtk.iconTheme.package = gruvboxPlus;
+  # gtk.iconTheme.name = "GruvboxPlus";
+
 }
